e7f7c5e5af88ed14e9569004e30574991958d22f
davydovct <davydovct@cleantalk.org>
2020-07-23 12:46:10 +0500

    Fix get_fields_any

--- cleantalk.antispam/lib/Cleantalk/Common/Helper.php
+++ cleantalk.antispam/lib/Cleantalk/Common/Helper.php
@@ -622,31 +622,32 @@ class Helper
 
 		//Skip request if fields exists
 		$skip_params = array(
-			'ipn_track_id',    // PayPal IPN #
-			'txn_type',        // PayPal transaction type
-			'payment_status',    // PayPal payment status
-			'ccbill_ipn',        // CCBill IPN
-			'ct_checkjs',        // skip ct_checkjs field
+		    'ipn_track_id', 	// PayPal IPN #
+		    'txn_type', 		// PayPal transaction type
+		    'payment_status', 	// PayPal payment status
+		    'ccbill_ipn', 		// CCBill IPN 
+			'ct_checkjs', 		// skip ct_checkjs field
 			'api_mode',         // DigiStore-API
-			'loadLastCommentId', // Plugin: WP Discuz. ticket_id=5571
-		);
-
+			'loadLastCommentId' // Plugin: WP Discuz. ticket_id=5571
+	    );
+		
 		// Fields to replace with ****
-		$obfuscate_params = array(
-			'password',
-			'pass',
-			'pwd',
+	    $obfuscate_params = array(
+	        'password',
+	        'pass',
+	        'pwd',
 			'pswd'
-		);
-
+	    );
+		
 		// Skip feilds with these strings and known service fields
-		$skip_fields_with_strings = array(
+		$skip_fields_with_strings = array( 
 			// Common
 			'ct_checkjs', //Do not send ct_checkjs
 			'nonce', //nonce for strings such as 'rsvp_nonce_name'
 			'security',
 			// 'action',
 			'http_referer',
+			'referer-page',
 			'timestamp',
 			'captcha',
 			// Formidable Form
@@ -671,13 +672,13 @@ class Helper
 			'formData_id',
 			'formData_settings',
 			'formData_fields_\d+_id',
-			'formData_fields_\d+_files.*',
+			'formData_fields_\d+_files.*',		
 			// E_signature
 			'recipient_signature',
 			'output_\d+_\w{0,2}',
 			// Contact Form by Web-Settler protection
-			'_formId',
-			'_returnLink',
+	        '_formId',
+	        '_returnLink',
 			// Social login and more
 			'_save',
 			'_facebook',
@@ -685,171 +686,149 @@ class Helper
 			'user_login-',
 			// Contact Form 7
 			'_wpcf7',
-			'avatar__file_image_data',
-			'task',
-			'page_url',
-			'page_title',
-			'Submit',
-			'formId',
-			'key',
-			'id',
-			'hiddenlists',
-			'ctrl',
-			'task',
-			'option',
-			'nextstep',
-			'acy_source',
-			'subid',
-			'ct_action',
-			'ct_method',
+			'ebd_settings',
+			'ebd_downloads_',
+			'ecole_origine',
+			'signature',
 		);
-
+		
 		// Reset $message if we have a sign-up data
-		$skip_message_post = array(
-			'edd_action', // Easy Digital Downloads
-		);
-
+	    $skip_message_post = array(
+	        'edd_action', // Easy Digital Downloads
+	    );
+		
 		foreach ($skip_params as $value)
 		{
 			if (@array_key_exists($value, $_GET) || @array_key_exists($value, $_POST))
 				$contact = false;
 		}
 		unset($value);
-
-		if (count($arr))
-		{
-			foreach ($arr as $key => $value)
-			{
-
-				if (gettype($value) == 'string')
-				{
-					$decoded_json_value = json_decode($value, true);
-					if ($decoded_json_value !== null)
+		
+		if(count($arr)){
+			
+			foreach($arr as $key => $value){
+							
+				if(gettype($value) == 'string'){
+					
+					$tmp = strpos($value, '\\') !== false ? stripslashes($value) : $value;
+					$decoded_json_value = json_decode($tmp, true);
+					
+					// Decoding JSON
+					if($decoded_json_value !== null){
 						$value = $decoded_json_value;
-				}
-
-				if (!is_array($value) && !is_object($value))
-				{
-
-					if (in_array($key, $skip_params, true) && $key != 0 && $key != '' || preg_match("/^ct_checkjs/", $key))
-						$contact = false;
-
-					if ($value === '')
-						continue;
-
-					// Skipping fields names with strings from (array)skip_fields_with_strings
-					foreach ($skip_fields_with_strings as $needle)
-					{
-						if (preg_match("/" . $needle . "/", $prev_name . $key) == 1)
-						{
-							continue(2);
+						
+					// Ajax Contact Forms. Get data from such strings:
+						// acfw30_name %% Blocked~acfw30_email %% s@cleantalk.org
+						// acfw30_textarea %% msg
+					}elseif(preg_match('/^\S+\s%%\s\S+.+$/', $value)){
+						$value = explode('~', $value);
+						foreach ($value as &$val){
+							$tmp = explode(' %% ', $val);
+							$val = array($tmp[0] => $tmp[1]);
 						}
 					}
-					unset($needle);
-
+				}
+				
+				if(!is_array($value) && !is_object($value)){
+					
+					if (in_array($key, $skip_params, true) && $key != 0 && $key != '' || preg_match("/^ct_checkjs/", $key))
+						$contact = false;
+					
+					if($value === '')
+						continue;
+					
+					// Skipping fields names with strings from (array)skip_fields_with_strings
+					foreach($skip_fields_with_strings as $needle){
+						if (preg_match("/".$needle."/", $prev_name.$key) == 1){
+							continue(2);
+						}
+					}unset($needle);
+					
 					// Obfuscating params
-					foreach ($obfuscate_params as $needle)
-					{
-						if (strpos($key, $needle) !== false)
-						{
+					foreach($obfuscate_params as $needle){
+						if (strpos($key, $needle) !== false){
 							$value = self::obfuscate_param($value);
 							continue(2);
 						}
-					}
-					unset($needle);
+					}unset($needle);
 
-
-					// Removes whitespaces
-					$value = urldecode( trim( $value ) ); // Fully cleaned message
-					$value_for_email = trim( $value );    // Removes shortcodes to do better spam filtration on server side.
-
+	                $value_for_email = trim( strip_shortcodes( $value ) );    // Removes shortcodes to do better spam filtration on server side.
+					
 					// Email
 					if ( ! $email && preg_match( "/^\S+@\S+\.\S+$/", $value_for_email ) ) {
 						$email = $value_for_email;
 
-						// Names
-					} elseif (preg_match("/name/i", $key)) {
-
-						preg_match("/((name.?)?(your|first|for)(.?name)?)$/", $key, $match_forename);
-						preg_match("/((name.?)?(last|family|second|sur)(.?name)?)$/", $key, $match_surname);
-						preg_match("/^(name.?)?(nick|user)(.?name)?$/", $key, $match_nickname);
-
-						if (count($match_forename) > 1)
+	                // Removes whitespaces
+	                $value = urldecode( trim( strip_shortcodes( $value ) ) ); // Fully cleaned message
+						
+					// Names
+					}elseif (preg_match("/name/i", $key)){
+						
+						preg_match("/((name.?)?(your|first|for)(.?name)?)/", $key, $match_forename);
+						preg_match("/((name.?)?(last|family|second|sur)(.?name)?)/", $key, $match_surname);
+						preg_match("/(name.?)?(nick|user)(.?name)?/", $key, $match_nickname);
+						
+						if(count($match_forename) > 1)
 							$nickname['first'] = $value;
-						elseif (count($match_surname) > 1)
+						elseif(count($match_surname) > 1)
 							$nickname['last'] = $value;
-						elseif (count($match_nickname) > 1)
+						elseif(count($match_nickname) > 1)
 							$nickname['nick'] = $value;
 						else
-							$nickname[$prev_name . $key] = $value;
-
-						// Subject
-					}
-					elseif ($subject === null && preg_match("/subject/i", $key))
-					{
+							$message[$prev_name.$key] = $value;
+							
+					// Subject
+					}elseif ($subject === null && preg_match("/subject/i", $key)){
 						$subject = $value;
-
-						// Message
+					
+					// Message
+					}else{
+						$message[$prev_name.$key] = $value;					
 					}
-					else
-					{
-						$message[$prev_name . $key] = $value;
-					}
-
-				}
-				elseif (!is_object($value))
-				{
-
+					
+				}elseif(!is_object($value)){
+					
 					$prev_name_original = $prev_name;
-					$prev_name          = ($prev_name === '' ? $key . '_' : $prev_name . $key . '_');
-
+					$prev_name = ($prev_name === '' ? $key.'_' : $prev_name.$key.'_');
+					
 					$temp = self::get_fields_any($value, $message, $email, $nickname, $subject, $contact, $prev_name);
-
-					$message  = $temp['message'];
-					$email    = ($temp['email'] ? $temp['email'] : null);
-					$nickname = ($temp['nickname'] ? $temp['nickname'] : null);
-					$subject  = ($temp['subject'] ? $temp['subject'] : null);
-					if ($contact === true)
+					
+					$message 	= $temp['message'];
+					$email 		= ($temp['email'] 		? $temp['email'] : null);
+					$nickname 	= ($temp['nickname'] 	? $temp['nickname'] : null);				
+					$subject 	= ($temp['subject'] 	? $temp['subject'] : null);
+					if($contact === true)
 						$contact = ($temp['contact'] === false ? false : true);
-					$prev_name = $prev_name_original;
+					$prev_name 	= $prev_name_original;
 				}
-			}
-			unset($key, $value);
+			} unset($key, $value);
 		}
-
-		foreach ($skip_message_post as $v)
-		{
-			if (isset($_POST[$v]))
-			{
-				$message = null;
-				break;
-			}
-		}
-		unset($v);
-
+		
+	    foreach ($skip_message_post as $v) {
+	        if (isset($_POST[$v])) {
+	            $message = null;
+	            break;
+	        }
+	    } unset($v);
+		
 		//If top iteration, returns compiled name field. Example: "Nickname Firtsname Lastname".
-		if ($prev_name === '')
-		{
-			if (!empty($nickname))
-			{
+		if($prev_name === ''){
+			if(!empty($nickname)){
 				$nickname_str = '';
-				foreach ($nickname as $value)
-				{
-					$nickname_str .= ($value ? $value . " " : "");
-				}
-				unset($value);
+				foreach($nickname as $value){
+					$nickname_str .= ($value ? $value." " : "");
+				}unset($value);
 			}
 			$nickname = $nickname_str;
 		}
-
-		$return_param = array(
-			'email'    => $email,
-			'nickname' => $nickname,
-			'subject'  => $subject,
-			'contact'  => $contact,
-			'message'  => $message
-		);
-
+		
+	    $return_param = array(
+			'email' 	=> $email,
+			'nickname' 	=> $nickname,
+			'subject' 	=> $subject,
+			'contact' 	=> $contact,
+			'message' 	=> $message
+		);	
 		return $return_param;
 	}
 
