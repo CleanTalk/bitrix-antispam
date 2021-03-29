7f20b40967b185c62b5a396ac30b2882ea591317
Artem Davydov <yozzeebeatz@gmail.com>
2021-03-29 14:08:46 +0500

    Fix: skip reset password checking.

--- cleantalk.antispam/include.php
+++ cleantalk.antispam/include.php
@@ -250,7 +250,8 @@ class CleantalkAntispam {
                     ( isset( $_SERVER['USER_AGENT'] ) && strpos( $_SERVER['USER_AGENT'], '.NET Framework' )              !== false ) ||
                     ( isset( $_SERVER['USER_AGENT'] ) && strpos( $_SERVER['USER_AGENT'], 'Bitrix Telephony Controller' ) !== false ) || 
                     (isset($_POST['comment_post_id'], $_POST['comment'], $_POST['blog_upload_cid'])) || 
-                    strpos($_SERVER['REQUEST_URI'], '/order/make')
+                    strpos($_SERVER['REQUEST_URI'], '/order/make') ||
+                    (isset($_POST['NEW_PASSWORD'], $_POST['NEW_PASSWORD_CONFIRM']) && strpos($_SERVER['REQUEST_URI'], 'personal/profile/') !== false)
                 )
                 {
                     return;
