58a76086a8c013ee168c6f2f98c6811135ae3b2f
Artem Anoshin <artem-anoshin@yandex.ru>
2021-06-11 15:54:47 +0400

    Fix: added isset in /Common/Helper.php in http__get_headers()

--- cleantalk.antispam/lib/Cleantalk/Common/Helper.php
+++ cleantalk.antispam/lib/Cleantalk/Common/Helper.php
@@ -1262,7 +1262,9 @@ class Helper
                 if(count($key_parts) > 0 and strlen($server_key) > 2){
                     foreach($key_parts as $part_index => $part){
                         $key_parts[$part_index] = function_exists('mb_strtolower') ? mb_strtolower($part) : strtolower($part);
-                        $key_parts[$part_index][0] = strtoupper($key_parts[$part_index][0]);
+                        if(!empty($key_parts[$part_index][0])) {
+							$key_parts[$part_index][0] = strtoupper($key_parts[$part_index][0]);
+						}
                     }
                     $server_key = implode('-', $key_parts);
                 }
