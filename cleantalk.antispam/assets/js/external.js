eeb40c0b9a643988d387511e51734516423e25a5
alexandergull <alex.g@cleantalk.org>
2025-10-01 23:55:12 +0500

    Upd. JS. External forms. Refactored.

--- cleantalk.antispam/assets/js/external.js
+++ cleantalk.antispam/assets/js/external.js
@@ -16,11 +16,6 @@ function ctProtectExternal() {
     for (let i = 0; i < document.forms.length; i++) {
         const currentForm = document.forms[i];
 
-        // skip excluded forms
-        // if (formIsExclusion(currentForm)) {
-        //     continue;
-        // }
-
         // Ajax checking for the integrated forms - will be changed the whole form object to make protection
         if (isIntegratedForm(currentForm)) {
             apbctProcessExternalForm(currentForm, i, document);
@@ -119,7 +114,7 @@ function apbctProcessExternalForm(currentForm, iterator, documentObject) {
  */
 function apbctReplaceInputsValuesFromOtherForm(sourceForm, targetForm) {
     if (!sourceForm || !targetForm) return;
-    
+
     const sourceInputs = sourceForm.querySelectorAll('input, textarea, select');
     sourceInputs.forEach(function(sourceInput) {
         if (sourceInput.name && sourceInput.type !== 'submit' && sourceInput.type !== 'button') {
@@ -135,250 +130,197 @@ function apbctReplaceInputsValuesFromOtherForm(sourceForm, targetForm) {
     });
 }
 
-/**
- * ApbctHandler class for handling block messages
- */
-function ApbctHandler() {
-    this.parseBlockMessage = function(result) {
-        let message = '';
-        if (result.apbct && result.apbct.comment) {
-            message = result.apbct.comment;
-        } else if (result.error && result.error.msg) {
-            message = result.error.msg;
-        } else if (result.data && result.data.message) {
-            message = result.data.message;
+function apbctParseBlockMessageForAajax(result) {
+    let message = '';
+    console.table('result',result)
+    if (result.apbct && result.apbct.comment) {
+        message = result.apbct.comment;
+    } else if (result.error && result.error.msg) {
+        message = result.error.msg;
+    } else if (result.data && result.data.message) {
+        message = result.data.message;
+    }
+    console.table('message',message)
+    if (message) {
+        alert(message);
+        if (result.apbct && result.apbct.stop_script == 1) {
+            window.stop();
         }
-        
-        if (message) {
-            alert(message);
-            if (result.apbct && result.apbct.stop_script == 1) {
-                window.stop();
-            }
-        }
-    };
+    }
 }
-
 /**
  * Sending Ajax for checking form data
  * @param {HTMLElement} form
- * @param {HTMLElement} prev
- * @param {HTMLElement} formOriginal
  */
 function sendAjaxCheckingFormData(form) {
-    let botDetectorEventToken = localStorage.getItem('bot_detector_event_token');
-    if (typeof botDetectorEventToken === 'string') {
-        botDetectorEventToken = JSON.parse(botDetectorEventToken);
-        if (typeof botDetectorEventToken === 'object') {
-            botDetectorEventToken = botDetectorEventToken.value;
+    const botDetectorEventToken = getBotDetectorToken();
+    const data = prepareFormData(form, botDetectorEventToken);
+
+    if (typeof BX !== 'undefined' && BX.ajax) {
+        sendBitrixAjax(form, data);
+    } else {
+        sendNativeAjax(form, data);
+    }
+}
+
+/**
+ * Get bot detector token from localStorage
+ */
+function getBotDetectorToken() {
+    let token = localStorage.getItem('bot_detector_event_token');
+    if (typeof token === 'string') {
+        token = JSON.parse(token);
+        if (typeof token === 'object') {
+            return token.value;
         }
     }
-    const data = {
-        'ct_bot_detector_event_token': botDetectorEventToken,
-    };
-    let elems = form.elements;
-    elems = Array.prototype.slice.call(elems);
+    return token;
+}
 
-    elems.forEach(function(elem, y) {
-        elem.name === '' ? data['input_' + y] = elem.value : data[elem.name] = elem.value;
+/**
+ * Prepare form data for submission
+ */
+function prepareFormData(form, botDetectorToken) {
+    const data = {
+        'ct_bot_detector_event_token': botDetectorToken,
+    };
+
+    const elements = Array.from(form.elements);
+    elements.forEach((elem, index) => {
+        const key = elem.name || `input_${index}`;
+        data[key] = elem.value;
     });
 
-    // Use Bitrix AJAX framework
-    if (typeof BX !== 'undefined' && BX.ajax) {
-        BX.ajax({
-            url: '/bitrix/components/cleantalk/ajax/ajax.php',
-            method: 'POST',
-            data: data,
-            dataType: 'json',
-            async: false,
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest'
-            },
-            onsuccess: function(result) {
-                console.log('CleanTalk AJAX success:', result);
-                if ((result.apbct === undefined && result.data === undefined) ||
-                    (result.apbct !== undefined && ! +result.apbct.blocked)
-                ) {
-                    // Clear service fields
-                    // for (const el of form.querySelectorAll('input[value="cleantalk_force_ajax_check"]')) {
-                    //     el.remove();
-                    // }
+    return data;
+}
 
-                    const formNew = form;
-                    form.parentElement.removeChild(form);
-                    const prev = form.apbctPrev;
-                    const formOriginal = form.apbctFormOriginal;
-                    // let isExternalFormsRestartRequired = apbctExternalFormsRestartRequired(formOriginal);
-                    apbctReplaceInputsValuesFromOtherForm(formNew, formOriginal);
+/**
+ * Send AJAX using Bitrix framework
+ */
+function sendBitrixAjax(form, data) {
+    BX.ajax({
+        url: '/bitrix/components/cleantalk/ajax/ajax.php',
+        method: 'POST',
+        data: data,
+        dataType: 'json',
+        async: false,
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest'
+        },
+        onsuccess: (result) => handleSuccess(form, result),
+        onfailure: (result) => handleFailure(form, result)
+    });
+}
 
-                    prev.after(formOriginal);
+/**
+ * Send AJAX using native XMLHttpRequest
+ */
+function sendNativeAjax(form, data) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/bitrix/components/cleantalk/ajax/ajax.php', false);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
 
-                    for (const el of formOriginal.querySelectorAll('input[value="cleantalk_force_ajax_check"]')) {
-                        el.remove();
-                    }
-                    for (const el of formOriginal.querySelectorAll('input[name="ct_bot_detector_event_token"]')) {
-                        el.remove();
-                    }
+    const formData = new URLSearchParams();
+    for (const key in data) {
+        formData.append(key, data[key]);
+    }
 
-                    // Common click event
-                    let submButton = formOriginal.querySelectorAll('button[type=submit]');
-                    if ( submButton.length !== 0 ) {
-                        console.log('CleanTalk AJAX success 1:', submButton);
-                        // submButton[0].click();
-                        // if (isExternalFormsRestartRequired) {
-                        //     setTimeout(function() {
-                        //         ctProtectExternal();
-                        //     }, 1500);
-                        // }
-                        return;
-                    }
+    xhr.send(formData);
 
-                    submButton = formOriginal.querySelectorAll('input[type=submit]');
-                    if ( submButton.length !== 0 ) {
-                        console.log('CleanTalk AJAX success 2:', submButton);
-                        // submButton[0].click();
-                        // if (isExternalFormsRestartRequired) {
-                        //     setTimeout(function() {
-                        //         ctProtectExternal();
-                        //     }, 1500);
-                        // }
-                        return;
-                    }
-                }
-                // if ((result.apbct !== undefined && +result.apbct.blocked) ||
-                //     (result.data !== undefined && result.data.message !== undefined)
-                // ) {
-                //     new ApbctHandler().parseBlockMessage(result);
-                // }
-            },
-            onfailure: function(result) {
-                console.error('CleanTalk AJAX error:', result);
-                // Allow form submission on error
-                const formNew = form;
-                form.parentElement.removeChild(form);
-                const prev = form.apbctPrev;
-                const formOriginal = form.apbctFormOriginal;
-                apbctReplaceInputsValuesFromOtherForm(formNew, formOriginal);
-                prev.after(formOriginal);
-                
-                for (const el of formOriginal.querySelectorAll('input[name="cleantalk_force_ajax_check"]')) {
-                    el.remove();
-                }
-                for (const el of formOriginal.querySelectorAll('input[name="ct_bot_detector_event_token"]')) {
-                    el.remove();
-                }
-                
-                let submButton = formOriginal.querySelectorAll('button[type=submit]');
-                if ( submButton.length !== 0 ) {
-                    console.log('CleanTalk AJAX error 1:', submButton);
-                    // submButton[0].click();
-                    return;
-                }
-                submButton = formOriginal.querySelectorAll('input[type=submit]');
-                if ( submButton.length !== 0 ) {
-                    console.log('CleanTalk AJAX error 2:', submButton);
-                    // submButton[0].click();
-                }
-            }
-        });
-    } else {
-        // Fallback to native XMLHttpRequest if BX is not available
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', '/bitrix/components/cleantalk/ajax/ajax.php', false); // synchronous
-        xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
-        
-        const formData = new URLSearchParams();
-        for (const key in data) {
-            formData.append(key, data[key]);
+    if (xhr.status === 200) {
+        try {
+            const result = JSON.parse(xhr.responseText);
+            handleSuccess(form, result);
+        } catch (e) {
+            console.error('CleanTalk JSON parse error:', e);
+            restoreAndSubmitForm(form);
         }
-        
-        xhr.send(formData);
-        
-        if (xhr.status === 200) {
-            try {
-                const result = JSON.parse(xhr.responseText);
-                if ((result.apbct === undefined && result.data === undefined) ||
-                    (result.apbct !== undefined && ! +result.apbct.blocked)
-                ) {
-                    // Allow form submission
-                    const formNew = form;
-                    form.parentElement.removeChild(form);
-                    const prev = form.apbctPrev;
-                    const formOriginal = form.apbctFormOriginal;
-                    apbctReplaceInputsValuesFromOtherForm(formNew, formOriginal);
-                    prev.after(formOriginal);
-                    
-                    for (const el of formOriginal.querySelectorAll('input[value="cleantalk_force_ajax_check"]')) {
-                        el.remove();
-                    }
-                    
-                    let submButton = formOriginal.querySelectorAll('button[type=submit]');
-                    if ( submButton.length !== 0 ) {
-                        console.log('CleanTalk AJAX success 1:', submButton);
-                        // submButton[0].click();
-                        return;
-                    }
-                    submButton = formOriginal.querySelectorAll('input[type=submit]');
-                    if ( submButton.length !== 0 ) {
-                        console.log('CleanTalk AJAX success 2:', submButton);
-                        // submButton[0].click();
-                    }
-                } else if ((result.apbct !== undefined && +result.apbct.blocked) ||
-                    (result.data !== undefined && result.data.message !== undefined)
-                ) {
-                    new ApbctHandler().parseBlockMessage(result);
-                }
-            } catch (e) {
-                console.error('CleanTalk JSON parse error:', e);
-                // Allow form submission on error
-                const formNew = form;
-                form.parentElement.removeChild(form);
-                const prev = form.apbctPrev;
-                const formOriginal = form.apbctFormOriginal;
-                apbctReplaceInputsValuesFromOtherForm(formNew, formOriginal);
-                prev.after(formOriginal);
-                
-                for (const el of formOriginal.querySelectorAll('input[value="cleantalk_force_ajax_check"]')) {
-                    el.remove();
-                }
-                
-                let submButton = formOriginal.querySelectorAll('button[type=submit]');
-                if ( submButton.length !== 0 ) {
-                    console.log('CleanTalk AJAX error 1:', submButton);
-                    // submButton[0].click();
-                    return;
-                }
-                submButton = formOriginal.querySelectorAll('input[type=submit]');
-                if ( submButton.length !== 0 ) {
-                    console.log('CleanTalk AJAX error 2:', submButton);
-                    // submButton[0].click();
-                }
-            }
-        } else {
-            console.error('CleanTalk AJAX request failed:', xhr.status);
-            // Allow form submission on error
-            const formNew = form;
-            form.parentElement.removeChild(form);
-            const prev = form.apbctPrev;
-            const formOriginal = form.apbctFormOriginal;
-            apbctReplaceInputsValuesFromOtherForm(formNew, formOriginal);
-            prev.after(formOriginal);
-            
-            for (const el of formOriginal.querySelectorAll('input[value="cleantalk_force_ajax_check"]')) {
-                el.remove();
-            }
-            
-            let submButton = formOriginal.querySelectorAll('button[type=submit]');
-            if ( submButton.length !== 0 ) {
-                console.log('CleanTalk AJAX success 1:', submButton);
-                // submButton[0].click();
-                return;
-            }
-            submButton = formOriginal.querySelectorAll('input[type=submit]');
-            if ( submButton.length !== 0 ) {
-                console.log('CleanTalk AJAX success 2:', submButton);
-                // submButton[0].click();
-            }
+    } else {
+        console.error('CleanTalk AJAX request failed:', xhr.status);
+        restoreAndSubmitForm(form);
+    }
+}
+
+/**
+ * Handle successful AJAX response
+ */
+function handleSuccess(form, result) {
+    console.log('CleanTalk AJAX success:', result);
+
+    const isAllowed = (result.apbct === undefined && result.data === undefined) ||
+        (result.apbct !== undefined && !+result.apbct.blocked);
+
+    const isBlocked = (result.apbct !== undefined && +result.apbct.blocked) ||
+        (result.data !== undefined && result.data.message !== undefined);
+
+    if (isAllowed) {
+        restoreAndSubmitForm(form, true);
+    } else if (isBlocked) {
+        apbctParseBlockMessageForAajax(result);
+    }
+}
+
+/**
+ * Handle AJAX failure
+ */
+function handleFailure(form, result) {
+    console.error('CleanTalk AJAX error:', result);
+    restoreAndSubmitForm(form);
+}
+
+/**
+ * Restore original form and trigger submission
+ */
+function restoreAndSubmitForm(form, isSuccess = false) {
+    const formNew = form;
+    const prev = form.apbctPrev;
+    const formOriginal = form.apbctFormOriginal;
+
+    // Remove current form and restore original
+    form.parentElement.removeChild(form);
+    apbctReplaceInputsValuesFromOtherForm(formNew, formOriginal);
+    prev.after(formOriginal);
+
+    // Clean up service fields
+    removeServiceFields(formOriginal);
+
+    // Find and trigger submit button
+    const submitButton = findSubmitButton(formOriginal);
+    if (submitButton) {
+        console.log(`CleanTalk AJAX ${isSuccess ? 'success' : 'error'}:`, submitButton);
+        submitButton.click();
+
+        // Schedule external forms protection for successful cases
+        if (isSuccess) {
+            setTimeout(() => {
+                ctProtectExternal();
+            }, 1500);
         }
     }
+}
+
+/**
+ * Remove service fields from form
+ */
+function removeServiceFields(form) {
+    const selectors = [
+        'input[value="cleantalk_force_ajax_check"]',
+        'input[name="ct_bot_detector_event_token"]'
+    ];
+
+    selectors.forEach(selector => {
+        form.querySelectorAll(selector).forEach(el => el.remove());
+    });
+}
+
+/**
+ * Find submit button in form
+ */
+function findSubmitButton(form) {
+    const button = form.querySelector('button[type="submit"]');
+    if (button) return button;
+
+    const input = form.querySelector('input[type="submit"]');
+    return input || null;
 }
